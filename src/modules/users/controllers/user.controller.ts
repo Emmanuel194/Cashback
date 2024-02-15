0769be74b57bab1081d79efa1576d58b81c616b2
emmanuel.lima <86535005+Emmanuel194@users.noreply.github.com>
2024-02-14 21:56:43 -0300

    atualizações

--- src/modules/users/controllers/user.controller.ts
+++ src/modules/users/controllers/user.controller.ts
@@ -1,9 +1,11 @@
-import { Request, Response } from "express";
+import { Request, Response, request } from "express";
 import * as bcrypt from "bcrypt";
 import User from "../entities/user.entity";
 import { AppDataSource } from "../../../../banco";
 import { DeepPartial } from "typeorm";
 import jwt from "jsonwebtoken";
+import console from "console";
+import * as nodemailer from "nodemailer";
 
 const userRepository = AppDataSource.getRepository(User);
 
@@ -41,6 +43,93 @@ export const createUser = async (req: Request, res: Response) => {
     return res
       .status(500)
       .json({ ok: false, error: "Error trying to create user. " });
+  }
+};
+// * Login
+
+export const login = async (request: Request, response: Response) => {
+  const { email, password } = request.body;
+
+  try {
+    const user = await AppDataSource.getRepository(User).find({
+      where: {
+        email,
+      },
+    });
+
+    if (await bcrypt.compare(password, user[0].password)) {
+      const data = {
+        id: user[0].id,
+        name: user[0].name,
+        email: user[0].email,
+      };
+      return response.json({ data });
+    } else {
+      return response.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    return response.status(422).json({ message: "Error in entities!" });
+  }
+};
+
+// *  Recuperação de senha
+
+// Função para verificar se o email existe
+export const checkEmailExists = async (req: Request, res: Response) => {
+  const { email } = request.body;
+  try {
+    const existingUser = await AppDataSource.getRepository(User).find({
+      where: {
+        email,
+      },
+    });
+    if (existingUser) {
+      return res.redirect("/email.controller.ts");
+    } else {
+      return res.status(404).json({ message: "Email not found" });
+    }
+  } catch (error) {
+    console.error("Error checking email:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// * Função para enviar email de confirmação
+export const sendPasswordResetEmail = async (req: Request, res: Response) => {
+  try {
+    const { email } = req.body;
+
+    // * Crie um token JWT com o email (substitua pela sua chave secreta real)
+    const secretKey = "KHBDASHD912731237N9S7";
+    const token = jwt.sign({ email }, secretKey, { expiresIn: "1h" });
+
+    // Configurações do Nodemailer (substitua pelas suas configurações reais)
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: "suportebellitate@gmail.com",
+        pass: "bellitate123",
+      },
+    });
+
+    // Opções do email
+    const mailOptions = {
+      from: "suportebellitate@gmail.com",
+      to: email,
+      subject: "Redefinição de Senha",
+      text: "Clique no link para redefinir sua senha",
+      html: `<p>Clique no link para redefinir sua senha!</p>${token}`,
+    };
+
+    // Envie o email
+    await transporter.sendMail(mailOptions);
+
+    return res
+      .status(200)
+      .json({ message: "Email de confirmação enviado com sucesso" });
+  } catch (error) {
+    console.error("Erro ao enviar email de confirmação:", error);
+    return res.status(500).json({ message: "Erro interno do servidor" });
   }
 };
 
